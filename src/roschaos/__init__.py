415ea8193c1b842dbb2da27fd1ebcdee92aff5fd
ruffsl <roxfoxpox@gmail.com>
2018-07-05 18:35:23 -0700

    Refactoring CLI

--- src/roschaos/__init__.py
+++ src/roschaos/__init__.py
@@ -3,27 +3,28 @@
 
 from __future__ import print_function
 
+# import errno
 import os
-import errno
+import re
 import sys
 import socket
-import time
+
+try:  # py3k
+    import urllib.parse as urlparse
+except ImportError:
+    import urlparse
+
+
+from argparse import ArgumentParser
+
 try:
     from xmlrpc.client import ServerProxy
 except ImportError:
     from xmlrpclib import ServerProxy
 
-try: #py3k
-    import urllib.parse as urlparse
-except ImportError:
-    import urlparse
-
-from argparse import ArgumentParser
-
-import rospy
 import rosgraph
-import rostopic
 import rosnode
+import rostopic
 
 from rospy.core import xmlrpcapi
 
@@ -33,49 +34,129 @@ ID = '/roschaos'
 TIMEOUT = 3.0
 
 
-def _roschaos_master_topic_unlist():
-    master = rosgraph.Master(ID)
-    rosnode.cleanup_master_whitelist(master, [])
+# def _roschaos_master_topic_scramble():
+#     master = rosgraph.Master(ID)
+#     master_rpc = xmlrpcapi(rosgraph.get_master_uri())
+#     nodes = rosnode.get_node_names(namespace=None)
+#     topic_types = rostopic._master_get_topic_types(master)
+#     for node_id in nodes:
+#         node_uri = rosnode.get_api_uri(master, node_id)
+#         for topic_name, topic_type in topic_types:
+#             pass
+#             code, msg, val = master_rpc.registerPublisher(node_id, topic_name, topic_type, node_uri)
+#             if code != 1:
+#                 print("cannot register publication topic [%s] with master: %s" % (topic_name, msg))
 
 
-def _roschaos_master_topic_scramble():
-    master = rosgraph.Master(ID)
-    master_rpc = xmlrpcapi(rosgraph.get_master_uri())
-    nodes = rosnode.get_node_names(namespace=None)
-    topic_types = rostopic._master_get_topic_types(master)
-    for node_id in nodes:
-        node_uri = rosnode.get_api_uri(master, node_id)
-        for topic_name, topic_type in topic_types:
-            pass
-            code, msg, val = master_rpc.registerPublisher(node_id, topic_name, topic_type, node_uri)
-            if code != 1:
-                print("cannot register publication topic [%s] with master: %s" % (topic_name, msg))
-
-
-def _roschaos_cmd_master(argv):
+def _roschaos_cmd_master(argv, parser):
     """
     Implements roschaos 'master' command.
     @param argv: command-line args
     @type  argv: [str]
     """
-    args = argv[2:]
-    parser = ArgumentParser(prog=NAME)
-    subparsers = parser.add_subparsers(help='Master Subcommands')
-    topic_parser = subparsers.add_parser(
-        'topic', help='Modify Topic Registration')
-    topic_parser.add_argument(
-        '--scramble',
-        action='store_true',
-        help='Scramble all topic registration')
-    topic_parser.add_argument(
-        '--unlist',
-        action='store_true',
-        help='Unlist all topic registration')
+    subparsers = parser.add_subparsers(help='master subcommands', dest='master')
+    unregister_parser = subparsers.add_parser(
+        'unregister', help='Remove Registration')
+    subsubparsers = unregister_parser.add_subparsers(
+        help='unregister subcommands',
+        dest='unregister')
 
-    if options.scramble:
-        _roschaos_master_topic_scramble()
-    if options.unlist:
-        _roschaos_master_topic_unlist()
+    unregister_node_parser = subsubparsers.add_parser(
+        'node', help='remove node registration')
+    unregister_node_parser.add_argument(
+        '--name',
+        action='store',
+        help='name expression')
+    unregister_node_parser.add_argument(
+        '--uri',
+        action='store',
+        help='uri expression')
+    unregister_node_parser.add_argument(
+        '--all',
+        action='store_true',
+        help='all nodes')
+
+    unregister_service_parser = subsubparsers.add_parser(
+        'service', help='remove service registration')
+    unregister_service_parser.add_argument(
+        '--name',
+        action='store',
+        help='name expression')
+
+    unregister_topic_parser = subsubparsers.add_parser(
+        'topic', help='remove topic registration')
+    unregister_topic_parser.add_argument(
+        '--name',
+        action='store',
+        help='name expression')
+    unregister_topic_parser.add_argument(
+        '--type',
+        action='store',
+        help='type expression')
+    unregister_topic_parser.add_argument(
+        '--subscribers',
+        action='store_true',
+        help='subscribers only')
+    unregister_topic_parser.add_argument(
+        '--publishers',
+        action='store_true',
+        help='publishers only')
+    options, _ = parser.parse_known_args(argv)
+
+    if options.master == 'unregister':
+        if options.unregister == 'node':
+            if options.all:
+                _master_unregister_all_nodes()
+            else:
+                if not (options.name or options.uri):
+                    parser.error('No action requested')
+                _master_unregister_nodes(options.name, options.uri)
+        elif options.unregister == 'service':
+            _master_unregister_services(options.name)
+        elif options.unregister == 'topic':
+            _roschaos_master_unregister_topic(options)
+
+
+def _master_unregister_all_nodes():
+    master = rosgraph.Master(ID)
+    rosnode.cleanup_master_whitelist(master, [])
+
+
+def _master_unregister_nodes(name_str, uri_str):
+    name_pat = re.compile(name_str) if name_str else None
+    uri_pat = re.compile(uri_str) if uri_str else None
+
+    master = rosgraph.Master(ID)
+    master_rpc = xmlrpcapi(rosgraph.get_master_uri())
+    nodes = rosnode.get_node_names(namespace=None)
+    topic_types = rostopic._master_get_topic_types(master)
+    blacklisted_nodes = []
+    for node_name in nodes:
+        if name_pat:
+            if not name_pat.match(node_name):
+                continue
+        node_uri = rosnode.get_api_uri(master, node_name)
+        if uri_pat:
+            if not uri_pat.match(node_uri):
+                continue
+        blacklisted_nodes.append(node_name)
+    rosnode.cleanup_master_blacklist(master, blacklisted_nodes)
+
+
+def _master_unregister_services(name_str):
+    name_pat = re.compile(name_str) if name_str else None
+
+    master = rosgraph.Master(ID)
+    _, _, services = master.getSystemState()
+    for service_name, nodes in services:
+        if name_pat:
+            if not name_pat.match(service_name):
+                continue
+        for node_name in nodes:
+            service_api = master.lookupService(service_name)
+            master_n = rosgraph.Master(node_name)
+            master_n.unregisterService(service_name, service_api)
+            print("Unregistering {} {}".format(service_name, node_name))
 
 
 def _roschaos_node_master_backtrace(node_api):
@@ -108,17 +189,7 @@ def _roschaos_cmd_node(argv):
         _roschaos_node_master_backtrace(options.backtrace)
 
 
-def _roschaos_master_param_scramble():
-    # TODO
-    pass
-
-
-def _roschaos_master_param_unlist():
-    # TODO
-    pass
-
-
-def _roschaos_cmd_param(argv):
+def _roschaos_cmd_param(argv, parser):
     """
     Implements roschaos 'param' command.
     @param argv: command-line args
@@ -144,17 +215,6 @@ def _roschaos_cmd_param(argv):
     if options.unlist:
         _roschaos_master_param_unlist()
 
-def _fullusage():
-    print("""roschaos is a command-line tool for pentesting ROS APIs.
-Commands:
-\troschaos master topic --scramble\tscramble all topic registration
-\troschaos master topic --unlist\t\tunlist all topic registration
-\troschaos param server --scramble\tscramble all param registration
-\troschaos param server --unlist\t\tunlist all param registration
-\troschaos node master --backtrace\tbacktrace Master URI remotly from node
-Type roschaos <command> -h for more detailed usage, e.g. 'roschaos master topic -h'
-""")
-    sys.exit(getattr(os, 'EX_USAGE', 1))
 
 def roschaosmain(argv=None):
     """
@@ -162,32 +222,39 @@ def roschaosmain(argv=None):
     @param argv: override sys.argv
     @param argv: [str]
     """
-    if argv == None:
-        argv = sys.argv
-    if len(argv) == 1:
-        _fullusage()
+    if argv is None:
+        argv = sys.argv[1:]
+
+    parser = ArgumentParser(prog=NAME)
+    subparsers = parser.add_subparsers(help='roschaos Subcommands', dest='roschaos')
+    master_parser = subparsers.add_parser(
+        'master', help='master API toolkit', add_help=False)
+    slave_parser = subparsers.add_parser(
+        'slave', help='slave API toolkit', add_help=False)
+    param_parser = subparsers.add_parser(
+        'param', help='param API toolkit', add_help=False)
+    options, _ = parser.parse_known_args(argv)
+    argv = argv[1:]
+
     try:
-        command = argv[1]
-        if command == 'master':
-            sys.exit(_roschaos_cmd_master(argv) or 0)
-        elif command == 'node':
-            sys.exit(_roschaos_cmd_node(argv) or 0)
-        elif command == 'param':
-            sys.exit(_roschaos_cmd_param(argv) or 0)
-        elif command == '--help':
-            _fullusage(False)
-        else:
-            _fullusage()
+        if options.roschaos == 'master':
+            sys.exit(_roschaos_cmd_master(argv, master_parser) or 0)
+        elif options.roschaos == 'slave':
+            sys.exit(_roschaos_cmd_slave(argv, slave_parser) or 0)
+        elif options.roschaos == 'param':
+            sys.exit(_roschaos_cmd_param(argv, param_parser) or 0)
     except socket.error:
-        print("Network communication failed. Most likely failed to communicate with master.", file=sys.stderr)
+        print("Network communication failed." +
+              " Most likely failed to communicate with master.", file=sys.stderr)
         sys.exit(1)
-    except rosgraph.MasterError as e:
-        print("ERROR: "+str(e), file=sys.stderr)
+    except rosgraph.MasterError as err:
+        print("ERROR: "+str(err), file=sys.stderr)
         sys.exit(1)
-    except rosnode.ROSNodeException as e:
-        print("ERROR: "+str(e), file=sys.stderr)
+    except rosnode.ROSNodeException as err:
+        print("ERROR: "+str(err), file=sys.stderr)
         sys.exit(1)
-    except rostopic.ROSTopicException as e:
-        sys.stderr.write("ERROR: %s\n"%str(e))
+    except rostopic.ROSTopicException as err:
+        sys.stderr.write("ERROR: %s\n" % str(err))
         sys.exit(1)
-    except KeyboardInterrupt: pass
+    except KeyboardInterrupt:
+        pass
